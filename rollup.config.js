18bdea2f5c867380b66aa05cf8b3c728f9c7e788
a-bishop <andrew.bishop53@gmail.com>
2019-10-11 20:43:54 -0700

    Add index.js to top level

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
     input: ['src/index.js'],
     output: {
-        file: 'build/index.js',
+        file: 'index.js',
         format: 'es',
         sourcemap: true
     },
