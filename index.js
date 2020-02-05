bda4c28faa2535cbc151155b28fdc9f07ec238cd
a-bishop <andrew.bishop53@gmail.com>
2020-02-05 12:39:00 -0800

    Run rollup

--- index.js
+++ index.js
@@ -2959,7 +2959,6 @@ class DrumMachine extends LitElement {
       }
       return `${notes[newIndex]}${this.octave}`;
     });
-    console.log(this.scale, this.currScaleWithOctave);
 
     this.transportBeatStyle = {
       background: 'white',
@@ -2980,20 +2979,24 @@ class DrumMachine extends LitElement {
     // Snare setup
     const snare = new Tone.NoiseSynth({
       noise: {
-        type: 'brown',
-        playbackRate: 3
+        type: 'brown'
       },
       envelope: {
-        attack: 0.001,
-        decay: 0.13,
-        sustain: 0,
-        release: 0.03
+        attack: 0.005,
+        decay: 0.1
       }
-    }).toMaster();
+    });
+    const tom = new Tone.MembraneSynth();
+    const compressor = new Tone.MidSideCompressor();
+    const gain = new Tone.Gain();
+    snare.chain(compressor, gain);
+    tom.chain(compressor, gain);
+    gain.chain(Tone.Master);
 
     this.snareSeq = new Tone.Sequence(
       function(time, note) {
-        snare.triggerAttackRelease(note, time);
+        snare.triggerAttackRelease(time);
+        tom.triggerAttackRelease(time);
       },
       [null, null, null, null, null, null, null, null],
       '8n'
@@ -3004,7 +3007,10 @@ class DrumMachine extends LitElement {
       noise: {
         type: 'pink'
       }
-    }).toMaster();
+    });
+    var filter = new Tone.Filter(800, 'bandpass');
+    hiHat.chain(filter, compressor, Tone.Master);
+
     this.hiHatSeq = new Tone.Sequence(
       function(time, note) {
         hiHat.triggerAttackRelease(note, time);
@@ -3026,7 +3032,6 @@ class DrumMachine extends LitElement {
     autoFilter.start();
 
     // Arpeggiator Setup
-    console.log(arpMovement['randomWalk']);
     const synth = new Tone.FMSynth({
       oscillator: {
         type: 'triangle'
@@ -3251,7 +3256,6 @@ class DrumMachine extends LitElement {
       return `${notes[newIndex]}${this.octave}`;
     });
     this.arpSeq.values = this.currScaleWithOctave;
-    console.log(this.scale, this.currScaleWithOctave);
   }
 
   handleArpNoteUpdate(e) {
@@ -3349,7 +3353,6 @@ class DrumMachine extends LitElement {
     Tone.Transport.stop();
     this.activeBeat = 'beatIndex0';
     this.removeTransportBeatStyle();
-    console.log(this.cleared);
   }
 
   clear() {
